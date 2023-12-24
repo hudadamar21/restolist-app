a770027e9aac987122ab54e4295dc020367f0549
hudadamar21 <hudadamar21@gmail.com>
2023-12-24 19:32:53 +0700

    add factory menus fromjson

--- lib/types/menu.dart
+++ lib/types/menu.dart
@@ -1,11 +1,21 @@
 class Menu {
   final String name;
   Menu({required this.name});
+
+  factory Menu.fromJson(Map<String, dynamic> menu) => Menu(name: menu['name']);
 }
 
 class Menus {
-  final Menu foods;
-  final Menu drinks;
+  List<Menu> foods;
+  List<Menu> drinks;
 
   Menus({required this.foods, required this.drinks});
+
+  factory Menus.fromJson(Map<String, dynamic> json) => Menus(
+        foods:
+            (json['foods'] as List).map((item) => Menu.fromJson(item)).toList(),
+        drinks: (json['drinks'] as List)
+            .map((item) => Menu.fromJson(item))
+            .toList(),
+      );
 }
