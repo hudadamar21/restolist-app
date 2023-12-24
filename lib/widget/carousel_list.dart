011784e88dd309599236a0fda53a1915f87c5d45
hudadamar21 <hudadamar21@gmail.com>
2023-12-24 19:31:51 +0700

    refactor: create carousel widget

---
+++ lib/widget/carousel_list.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:restoran/widget/progressive_image.dart';
+
+class CarouselList extends StatelessWidget {
+  final List list;
+  final String image;
+  final String price;
+  const CarouselList(
+      {super.key,
+      required this.list,
+      required this.image,
+      required this.price});
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(height: 250.0),
+      items: list.map<Widget>((item) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: AspectRatio(
+                        aspectRatio: 16 / 9,
+                        child: ProgressiveImage(
+                          image: image,
+                          height: 150,
+                          width: 300,
+                          fit: BoxFit.cover,
+                        )),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    item.name,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    price,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 5),
+                  const Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 16,
+                        color: Colors.orange,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        '5.0',
+                        style: TextStyle(fontSize: 14),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
